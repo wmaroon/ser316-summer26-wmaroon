19ed763216dbe0964358d03525179b819cf32624
William M <william.maroon@gmail.com>
2026-06-10 14:33:47 -0400

    Fix defect-8 Makes MAX_FINE_AMOUNT immutatble Closes #12

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * Handles book checkouts, returns, renewals, and fine calculations.
  */
 public class Checkout {
-    public static double MAX_FINE_AMOUNT = 25.0;
+    public static final double MAX_FINE_AMOUNT = 25.0;
 
     private Map<String, Book> bookList; // ISBN -> Book
     private Map<String, Patron> patrons; // PatronID -> Patron
